8c759b114b95d15419c2b6941bd8007fcf09c810
Daniel <hi@d2a.io>
2020-09-07 08:52:29 +0000

    Removed redundant code, added some documentation

--- Mathematik/wuerfel.py
+++ Mathematik/wuerfel.py
@@ -1,35 +1,40 @@
 from typing import Callable
 
-def iterate(seiten: list, anzahl: int, values: list = [], possible = []):
-    for seite in seiten:
+"""
+This function returns all possible cube combinations for the given sides and number of throws
+"""
+def iterate(dice_pages: list, num_throws: int, values: list = [], possible = []):
+    for page in dice_pages:
         tmp: list = values[:]
-        tmp.append(seite)
+        tmp.append(page)
         
-        if len(tmp) < anzahl:
-            iterate(seiten, anzahl, tmp)
+        if len(tmp) < num_throws:
+            iterate(dice_pages, num_throws, tmp)
         else:
             # for omega
             possible.append(tmp)
 
     return possible
 
-def do_check(wuerfe: int, check: Callable[[list], bool]):
-    seiten: list = [1, 2, 2, 3, 3, 3]
-    ergebnis: list = []
+"""
+This function checks all results of iterate against the given check function.
+Check should be a callable (anonymous) function.
+"""
+def do_check(num_throws: int, check: Callable[[list], bool]):
+    dice_pages: list = [1, 2, 2, 3, 3, 3]
+    result: list = []
 
-    omega: list = iterate(seiten, wuerfe)
+    omega: list = iterate(dice_pages, num_throws)
 
     for o in omega:
         if callable(check) and check(o):
-            ergebnis.append(o)
-        #if sum(o) <= 6:
-        #    ergebnis.append(o)
+            result.append(o)
 
-    pE = len(ergebnis) / len(omega)
+    pE = len(result) / len(omega)
 
-    print(f"#würfe = {wuerfe}")
+    print(f"#würfe = {num_throws}")
     print(f"|Ω| = {len(omega)}")
-    print(f"|E| = {len(ergebnis)}")
+    print(f"|E| = {len(result)}")
     print(f"P(E) = {pE} = {round(pE, 3)}%")
 
 do_check(4, lambda arr: sum(arr) < 6)
